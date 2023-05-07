e5b3d8cf47b8334e34d6330aa2ff16e72aa612a5
sorina3194 <sorinatudor94@gmail.com>
2023-05-07 14:27:57 +0200

    turn Gameboard into an object and input names

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -3,114 +3,130 @@ function Player(currentPlayer, marker) {
   this.marker = marker;
 }
 
-function Gameboard() {
-  const player1 = new Player("player1", "X");
-  const player2 = new Player("player2", "O");
-  let currentPlayer = player1;
-  const board = [
+function Gameboard(name1, name2) {
+  this.player1 = new Player(name1, "X");
+  this.player2 = new Player(name2, "O");
+  this.currentPlayer = this.player1;
+  this.board = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""],
   ];
-  const checkBoard = () => {
-    const winningStates = [
-      [
-        //row1
-        [0, 0],
-        [0, 1],
-        [0, 2],
-      ],
-      [
-        //row2
-        [1, 0],
-        [1, 1],
-        [1, 2],
-      ],
-      [
-        //row3
-        [2, 0],
-        [2, 1],
-        [2, 2],
-      ],
-      [
-        //column1
-        [0, 0],
-        [1, 0],
-        [2, 0],
-      ],
-      [
-        //column2
-        [0, 1],
-        [1, 1],
-        [2, 1],
-      ],
-      [
-        //column3
-        [0, 2],
-        [1, 2],
-        [2, 2],
-      ],
-      [
-        //diagonal1
-        [0, 0],
-        [1, 1],
-        [2, 2],
-      ],
-      [
-        //diagonal2
-        [0, 2],
-        [1, 1],
-        [2, 0],
-      ],
-    ];
-    for (let winState of winningStates) {
-      const [position1, position2, position3] = winState;
-      if (
-        board[position1[0]][position1[1]] !== "" &&
-        board[position1[0]][position1[1]] ===
-          board[position2[0]][position2[1]] &&
-        board[position1[0]][position1[1]] === board[position3[0]][position3[1]]
-      ) {
-        alert(`${currentPlayer.name} wins!`);
-        return;
-      }
-    }
-    const allSquaresUsed = board.every((row) =>
-      row.every((cell) => cell !== "")
-    );
-    if (allSquaresUsed) {
-      alert("Draw");
-    }
-  };
-  function drawBoard() {
-    let container = document.getElementById("container");
-    container.innerHTML = "";
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
-        const div = document.createElement("div");
-        container.appendChild(div);
-        div.innerHTML = board[i][j];
-        div.className = "cell";
-        div.addEventListener("click", function () {
-          if (board[i][j] === "") {
-            div.innerHTML = currentPlayer.marker;
-            board[i][j] = currentPlayer.marker;
-            checkBoard();
-            if (currentPlayer.name === player1.name) {
-              currentPlayer = player2;
-            } else {
-              currentPlayer = player1;
-            }
-            let playerX = document.getElementById("Player");
-            playerX.innerHTML = currentPlayer.marker;
-          } else {
-            console.log("Already played");
-          }
-        });
-      }
+}
+Gameboard.prototype.checkBoard = function () {
+  const winningStates = [
+    [
+      //row1
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ],
+    [
+      //row2
+      [1, 0],
+      [1, 1],
+      [1, 2],
+    ],
+    [
+      //row3
+      [2, 0],
+      [2, 1],
+      [2, 2],
+    ],
+    [
+      //column1
+      [0, 0],
+      [1, 0],
+      [2, 0],
+    ],
+    [
+      //column2
+      [0, 1],
+      [1, 1],
+      [2, 1],
+    ],
+    [
+      //column3
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ],
+    [
+      //diagonal1
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ],
+    [
+      //diagonal2
+      [0, 2],
+      [1, 1],
+      [2, 0],
+    ],
+  ];
+  for (let winState of winningStates) {
+    const [position1, position2, position3] = winState;
+    if (
+      this.board[position1[0]][position1[1]] !== "" &&
+      this.board[position1[0]][position1[1]] ===
+        this.board[position2[0]][position2[1]] &&
+      this.board[position1[0]][position1[1]] ===
+        this.board[position3[0]][position3[1]]
+    ) {
+      alert(`${this.currentPlayer.name} wins!`);
+      resetBoard();
+      return true;
     }
   }
-  drawBoard();
-}
+  const allSquaresUsed = this.board.every((row) =>
+    row.every((cell) => cell !== "")
+  );
+  if (allSquaresUsed) {
+    alert("Draw");
+    return true;
+  }
+  return false;
+};
 
-Gameboard();
+Gameboard.prototype.drawBoard = function () {
+  let container = document.getElementById("container");
+  container.innerHTML = "";
+  let playerX = document.getElementById("Player");
+  playerX.innerHTML = this.currentPlayer.name;
+
+  for (let i = 0; i < this.board.length; i++) {
+    for (let j = 0; j < this.board[i].length; j++) {
+      const div = document.createElement("div");
+      container.appendChild(div);
+      div.innerHTML = this.board[i][j];
+      div.className = "cell";
+      div.addEventListener("click", () => {
+        if (this.board[i][j] === "") {
+          div.innerHTML = this.currentPlayer.marker;
+          this.board[i][j] = this.currentPlayer.marker;
+          const finished = this.checkBoard();
+          if (finished === true) {
+            return;
+          }
+          if (this.currentPlayer.name === this.player1.name) {
+            this.currentPlayer = this.player2;
+          } else {
+            this.currentPlayer = this.player1;
+          }
+          let playerX = document.getElementById("Player");
+          playerX.innerHTML = this.currentPlayer.name;
+        } else {
+          console.log("Already played");
+        }
+      });
+    }
+  }
+};
+
+function resetBoard() {
+  const name1 = prompt("Insert player1 name:");
+  const name2 = prompt("Insert player2 name:");
+  const board = new Gameboard(name1, name2);
+  board.drawBoard();
+}
+resetBoard();
