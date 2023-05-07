825fa930c9a931418055d6eeaebe4b70939a214a
sorina3194 <sorinatudor94@gmail.com>
2023-05-07 13:21:58 +0200

    implement win status and draw

--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -12,6 +12,76 @@ function Gameboard() {
     ["", "", ""],
     ["", "", ""],
   ];
+  const checkBoard = () => {
+    const winningStates = [
+      [
+        //row1
+        [0, 0],
+        [0, 1],
+        [0, 2],
+      ],
+      [
+        //row2
+        [1, 0],
+        [1, 1],
+        [1, 2],
+      ],
+      [
+        //row3
+        [2, 0],
+        [2, 1],
+        [2, 2],
+      ],
+      [
+        //column1
+        [0, 0],
+        [1, 0],
+        [2, 0],
+      ],
+      [
+        //column2
+        [0, 1],
+        [1, 1],
+        [2, 1],
+      ],
+      [
+        //column3
+        [0, 2],
+        [1, 2],
+        [2, 2],
+      ],
+      [
+        //diagonal1
+        [0, 0],
+        [1, 1],
+        [2, 2],
+      ],
+      [
+        //diagonal2
+        [0, 2],
+        [1, 1],
+        [2, 0],
+      ],
+    ];
+    for (let winState of winningStates) {
+      const [position1, position2, position3] = winState;
+      if (
+        board[position1[0]][position1[1]] !== "" &&
+        board[position1[0]][position1[1]] ===
+          board[position2[0]][position2[1]] &&
+        board[position1[0]][position1[1]] === board[position3[0]][position3[1]]
+      ) {
+        alert(`${currentPlayer.name} wins!`);
+        return;
+      }
+    }
+    const allSquaresUsed = board.every((row) =>
+      row.every((cell) => cell !== "")
+    );
+    if (allSquaresUsed) {
+      alert("Draw");
+    }
+  };
   function drawBoard() {
     let container = document.getElementById("container");
     container.innerHTML = "";
@@ -25,11 +95,14 @@ function Gameboard() {
           if (board[i][j] === "") {
             div.innerHTML = currentPlayer.marker;
             board[i][j] = currentPlayer.marker;
+            checkBoard();
             if (currentPlayer.name === player1.name) {
               currentPlayer = player2;
             } else {
               currentPlayer = player1;
             }
+            let playerX = document.getElementById("Player");
+            playerX.innerHTML = currentPlayer.marker;
           } else {
             console.log("Already played");
           }
